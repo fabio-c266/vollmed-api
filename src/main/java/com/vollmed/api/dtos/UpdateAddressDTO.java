3ea612b36dc1853c106b89dec078139aae3bb36b
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-17 15:18:13 -0300

    refactor: messages

--- src/main/java/com/vollmed/api/dtos/UpdateAddressDTO.java
+++ src/main/java/com/vollmed/api/dtos/UpdateAddressDTO.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.Size;
 
 public record UpdateAddressDTO(
-        @Size(min = 8, max = 8, message = "CEP inválido")
+        @Size(min = 8, max = 8, message = "Ccep inválido")
         String cep,
 
-        @Size(min = 3, max = 100, message = "No mínimo 3 caracteris e no máximo 100")
+        @Size(min = 3, max = 100, message = "no mínimo 3 caracteris e no máximo 100")
         String street,
 
-        @Size(min = 3, max = 100, message = "No mínimo 3 caracteris e no máximo 100")
+        @Size(min = 3, max = 100, message = "no mínimo 3 caracteris e no máximo 100")
         String neighborhood,
 
-        @Size(max = 80, message = "O completo pode ter no máximo 80 caracteris")
+        @Size(max = 80, message = "o complemento pode ter no máximo 80 caracteris")
         String complement,
 
         @Size(min = 1)
