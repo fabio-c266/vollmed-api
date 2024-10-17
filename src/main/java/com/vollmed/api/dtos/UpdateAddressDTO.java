3d1dcdee1b5ab862a9cb4ead5b20564dbb6abab9
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-17 09:42:27 -0300

    feat: address update dto

---
+++ src/main/java/com/vollmed/api/dtos/UpdateAddressDTO.java
@@ -0,0 +1,24 @@
+package com.vollmed.api.dtos;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.Size;
+
+public record UpdateAddressDTO(
+        @Size(min = 8, max = 8, message = "CEP inválido")
+        String cep,
+
+        @Size(min = 3, max = 100, message = "No mínimo 3 caracteris e no máximo 100")
+        String street,
+
+        @Size(min = 3, max = 100, message = "No mínimo 3 caracteris e no máximo 100")
+        String neighborhood,
+
+        @Size(max = 80, message = "O completo pode ter no máximo 80 caracteris")
+        String complement,
+
+        @Size(min = 1)
+        int number,
+
+        @JsonAlias("city_name") String cityName
+) {
+}
