8a414e24114088ff17bace7c075dc33d0d76e6f9
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-17 15:15:19 -0300

    refactor: messages

--- src/main/java/com/vollmed/api/dtos/CreateAddressDTO.java
+++ src/main/java/com/vollmed/api/dtos/CreateAddressDTO.java
@@ -6,25 +6,25 @@ import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.Range;
 
 public record CreateAddressDTO(
-        @NotBlank(message = "Campo obrigatorio")
+        @NotBlank(message = "Campo obrigatório")
         @Size(min = 8, max = 8, message = "CEP inválido")
         String cep,
 
-        @NotBlank(message = "Campo obrigatorio")
+        @NotBlank(message = "Campo obrigatório")
         @Size(min = 3, max = 100, message = "No mínimo 3 caracteris e no máximo 100")
         String street,
 
-        @NotBlank(message = "Campo obrigatorio")
+        @NotBlank(message = "Campo obrigatório")
         @Size(min = 3, max = 100, message = "No mínimo 3 caracteris e no máximo 100")
         String neighborhood,
 
-        @Size(max = 80, message = "O completo pode ter no máximo 80 caracteris")
+        @Size(max = 80, message = "O complemento pode ter no máximo 80 caracteris")
         String complement,
 
         @Range(min = 1, message = "Precisa ser maior ou igual a 1")
         int number,
 
-        @NotBlank(message = "Campo obrigatorio")
+        @NotBlank(message = "Campo obrigatório")
         @JsonAlias("city_name") String cityName
         ) {
 }
