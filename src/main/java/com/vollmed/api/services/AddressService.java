f07dd2c8c029c0cf9dd4bd831fd4a72a8e523fde
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-17 15:13:35 -0300

    feat: added get by id and update methods

--- src/main/java/com/vollmed/api/services/AddressService.java
+++ src/main/java/com/vollmed/api/services/AddressService.java
@@ -3,9 +3,12 @@ package com.vollmed.api.services;
 import com.vollmed.api.domain.address.Address;
 import com.vollmed.api.domain.address.City;
 import com.vollmed.api.dtos.CreateAddressDTO;
+import com.vollmed.api.dtos.UpdateAddressDTO;
+import com.vollmed.api.exceptions.DTOEmptyException;
 import com.vollmed.api.exceptions.EntityNotFoundException;
 import com.vollmed.api.repositories.AddressRepository;
 import com.vollmed.api.repositories.CityRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +30,29 @@ public class AddressService {
 
         Address newAddress = new Address(createAddressDTO, city);
         return this.addressRepository.save(newAddress);
+    }
+
+    public Address getById(Long id) throws EntityNotFoundException {
+        return this.addressRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Address"));
+    }
+
+    @Transactional
+    public Address update(Long id, UpdateAddressDTO updateAddressDTO) throws EntityNotFoundException, DTOEmptyException {
+        Address address = this.getById(id);
+
+        if (updateAddressDTO == null) throw new DTOEmptyException();
+        if (updateAddressDTO.cep() != null) address.setCEP(updateAddressDTO.cep());
+        if (updateAddressDTO.street() != null) address.setStreet(updateAddressDTO.street());
+        if (updateAddressDTO.neighborhood() != null) address.setNeighborhood(updateAddressDTO.neighborhood());
+        if (updateAddressDTO.complement() != null) address.setComplement(updateAddressDTO.complement());
+        if (updateAddressDTO.number()  > 0) address.setNumber(updateAddressDTO.number());
+
+        if (updateAddressDTO.cityName() != null) {
+            City city = this.getCityByName(updateAddressDTO.cityName());
+            address.setCity(city);
+        }
+
+        return address;
     }
 }
