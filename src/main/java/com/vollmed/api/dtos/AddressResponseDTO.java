e80927d9b30fbdde1aa1677d073cd9bc1996965b
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-17 15:12:45 -0300

    feat: added constructor to build data

--- src/main/java/com/vollmed/api/dtos/AddressResponseDTO.java
+++ src/main/java/com/vollmed/api/dtos/AddressResponseDTO.java
@@ -1,5 +1,7 @@
 package com.vollmed.api.dtos;
 
+import com.vollmed.api.domain.address.Address;
+
 public record AddressResponseDTO(
         Long id,
         String cep,
@@ -9,4 +11,17 @@ public record AddressResponseDTO(
         int number,
         String city,
         String uf
-) {}
+) {
+    public AddressResponseDTO(Address address) {
+        this(
+                address.getId(),
+                address.getCEP(),
+                address.getStreet(),
+                address.getNeighborhood(),
+                address.getComplement(),
+                address.getNumber(),
+                address.getCity().getName(),
+                address.getCity().getUf().getName()
+        );
+    }
+}
