ed0c5da1cadbdaf1818303d28cb9c33b08614a0a
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-18 17:13:41 -0300

    refactor: message

--- src/main/java/com/vollmed/api/exceptions/ConflictException.java
+++ src/main/java/com/vollmed/api/exceptions/ConflictException.java
@@ -2,6 +2,6 @@ package com.vollmed.api.exceptions;
 
 public class ConflictException extends Exception {
     public ConflictException(String field) {
-        super("já possui um registro com esse " + field);
+        super("Já possui um registro com esse " + field);
     }
 }
